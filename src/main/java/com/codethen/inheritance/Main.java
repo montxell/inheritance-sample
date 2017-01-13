1adfa9e7019a6f23b5234886908f7e4757d33f07
Montse Ariso <montxelltech@gmail.com>
2017-01-13 18:35:07 +0100

    Use a list of machines

--- src/main/java/com/codethen/inheritance/Main.java
+++ src/main/java/com/codethen/inheritance/Main.java
@@ -1,18 +1,46 @@
 package com.codethen.inheritance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        //Machine fridge = new Fridge();
-        //Machine oven = new Oven();
+        Machine fridge = new Fridge();
+        Machine fridge2 = new Fridge();
+        Machine oven = new Oven();
+        Machine oven2 = new Oven();
 
-        Fridge fridge = new Fridge();
-        Oven oven = new Oven();
+        //Fridge fridge = new Fridge();
+        //Oven oven = new Oven();
 
-        fridge.turnOn();
-        oven.turnOn();
+        //fridge.turnOn();
+        //oven.turnOn();
+
+        //oven.increaseTemp(20);
+
+        List<Machine> list = new ArrayList<>();
+        list.add(oven);
+        list.add(oven2);
+        list.add(fridge);
+        list.add(fridge2);
+
+
+/*
+        for (int i=0; i<list.size(); i++) {
+            Machine machine = list.get(i);
+            System.out.println(i + " - ");
+            machine.turnOn();
+        }
+*/
+
+
+        for (Machine machine : list) {
+            machine.turnOn();
+        }
+
 
     }
 }
--- src/main/java/com/codethen/inheritance/Oven.java
+++ src/main/java/com/codethen/inheritance/Oven.java
@@ -3,10 +3,21 @@ package com.codethen.inheritance;
 public class Oven implements Machine {   // que hereda de clase Machine, se pone extends Machine
                                         // que hereda de interface Machine, se pone implements Machine
 
+    private int temperature;
+
+    public Oven(){
+        this.temperature = 0;
+    }
+
     public void turnOn() {
 
         System.out.println("Oven is on");
 
+    }
+
+
+    public void increaseTemp(int inc){
+        this.temperature += inc;
     }
 
 }
